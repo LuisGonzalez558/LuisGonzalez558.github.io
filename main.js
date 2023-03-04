64baf109628550d77eaa0f7cf27b271dcc586ed5
LuisGonzalez558 <a1120250002@utch.edu.mx>
2023-03-04 14:52:29 -0600

    Primis commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//Cargar Service Worker
+if('serviceWorker' in navigator){
+    console.log('Puedes usar el service worker')
+    //configuracion del sw
+    navigator.serviceWorker.register('./sw.js')
+                            .then(res=>console.log('SW cargado correctamente', res))
+                            .catch(err=>console.log('service Worker no se ha podido registrar',err));
+}
+else
+{
+    console.log('No se puede usar el Service Worker')
+}
---
+++ sw.js
@@ -0,0 +1,57 @@
+//Asignar nombre y version de la cache
+const CACHE_NAME='v1_cache_BCH_PWA';
+
+//configuracion de los ficheros
+var urlsToCache=[
+    './',
+    './windows11/StoreLogo.scale-125.png',
+    './windows11/StoreLogo.scale-150.png',
+    './windows11/StoreLogo.scale-200.png'
+];
+
+self.addEventListener('install', e=>{
+    e.WaitUntill(
+        caches.open(CACHE_NAME)
+              .then((cache)=>{
+                return cache.addAll(urlsToCache)
+                .then(()=>{
+                    self.skipWaiting();
+                })
+        })
+        .catch(err=>console.log('No se ha registrado el cache', err))
+    )
+});
+
+self.addEventListener('activate',e => {
+    const cacheWhiteList = [CACHE_NAME];
+    //que el evento espere a que termine de ejecutar
+    e.WaitUntill(
+        caches.keys()
+        .then(cacheNames=>{
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if(cacheWhiteList.indexOf(cacheName)== -1)
+                    {
+                        return cache.delete(cacheName);
+                    }
+                })
+            );
+        })
+        .then(()=>{
+            self.clients.claim();//activa la cache en el dispositivo
+        })
+    );
+})
+
+self.addEventListener('fetch',e => {
+    e.respondWith(
+        caches.match(e.request)
+        .then(res => {
+            if(res){
+                //devuelvo datos desde cache
+                return res;
+            }
+            return fetch(e.request);//hago peticion al servidor en caso de que no este en cache
+        })
+    );
+})
