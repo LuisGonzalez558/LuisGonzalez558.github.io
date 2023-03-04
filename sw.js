9300d8443896643e5c4a198aaf79e1ab830294e2
LuisGonzalez558 <a1120250002@utch.edu.mx>
2023-03-04 16:08:46 -0600

    8 commit

--- sw.js
+++ sw.js
@@ -10,9 +10,9 @@ var urlsToCache=[
 ];
 
 self.addEventListener('install', e=>{
-    e.WaitUntill(
+    e.waitUntill(
         caches.open(CACHE_NAME)
-              .then(cache=>{
+              .then(cache => {
                 return cache.addAll(urlsToCache)
                 .then(()=>{
                     self.skipWaiting();
@@ -25,7 +25,7 @@ self.addEventListener('install', e=>{
 self.addEventListener('activate',e => {
     const cacheWhiteList = [CACHE_NAME];
     //que el evento espere a que termine de ejecutar
-    e.WaitUntill(
+    e.waitUntill(
         caches.keys()
         .then(cacheNames=>{
             return Promise.all(
