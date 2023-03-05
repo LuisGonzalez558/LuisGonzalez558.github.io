0115968a406a7bd8068788237bf1d40ac8e716dc
LuisGonzalez558 <a1120250002@utch.edu.mx>
2023-03-04 21:15:30 -0600

    22 commit

--- sw.js
+++ sw.js
@@ -10,47 +10,51 @@ var urlsToCache=[
 ];
 
 self.addEventListener('install', e=>{
-    e.waitUntill(
+
+    e.waitUntil(
         caches.open(CACHE_NAME)
-              .then(cache => {
-                return cache.addAll(urlsToCache)
-                .then(()=> self.skipWaiting()
-                )
+        .then(cache => {
+            return cache.addAll(urlsToCache)
+                        .then(() =>{
+                            self.skipWaiting(); 
+                        })
         })
         .catch(err=>console.log('No se ha registrado el cache', err))
-    )
+);
 });
 
 self.addEventListener('activate',e => {
     const cacheWhitelist = [CACHE_NAME];
-    //que el evento espere a que termine de ejecutar
-    e.waitUntill(
+
+
+    e.waitUntil(
         caches.keys()
-        .then(cacheNames=>{
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if(cacheWhitelist.indexOf(cacheName)== -1)
-                    {
-                        return caches.delete(cacheName);
-                    }
-                })
-            )
-        })
-        .then(()=>{
-            self.clients.claim();//activa la cache en el dispositivo
-        })
-    )
+                .then(cacheNames=>{
+                return Promise.all(
+                    cacheNames.map(cacheName =>{
+                        if(cacheWhitelist.indexOf(cacheName)== -1)
+                        {
+
+                            return cache.delete(cacheName);
+                        }
+                    })
+                );
+               })
+               .then(()=> {
+                self.clients.claim(); 
+               })
+    );
+
 });
 
 self.addEventListener('fetch',e => {
     e.respondWith(
         caches.match(e.request)
-        .then(res => {
-            if(res){
-                //devuelvo datos desde cache
-                return res;
-            }
-            return fetch(e.request);//hago peticion al servidor en caso de que no este en cache
-        })
-    )
+                .then (res => {
+                    if(res){
+                        return res;
+                    }
+                    return fetch(e.request); 
+                })
+    );
 });
