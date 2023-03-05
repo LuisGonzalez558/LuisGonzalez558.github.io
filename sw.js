3db3c3b8a0b8885488669fb56ef96da1b548935d
LuisGonzalez558 <a1120250002@utch.edu.mx>
2023-03-04 22:15:00 -0600

    28 commit

--- sw.js
+++ sw.js
@@ -3,17 +3,25 @@ const CACHE_NAME = 'v1_cache_BCH_PWA';
 
 //configuracion de los ficheros
 var urlsToCache = [
-    './index.html',
-    './sw.js',
-    './main.js',
-    './manifest.js',
-    './windows11/StoreLogo.scale-125.png',
-    './windows11/StoreLogo.scale-150.png',
-    './windows11/StoreLogo.scale-200.png',
-    './windows11/Square44x44Logo.targetsize-256.png'
+    '/index.html',
+    '/sw.js',
+    '/main.js',
+    '/manifest.js',
+    '/windows11/StoreLogo.scale-125.png',
+    '/windows11/StoreLogo.scale-150.png',
+    '/windows11/StoreLogo.scale-200.png',
+    '/windows11/Square44x44Logo.targetsize-256.png'
 ];
 
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => { return cache.addAll(urlsToCache).then(() => { self.skipWaiting() }).catch(err => console.log('Hubo un error', err)) })); });
+self.addEventListener('install', event => { 
+    event.waitUntil(caches.open(CACHE_NAME)
+    .then(cache => { 
+        return cache
+        .addAll(urlsToCache)
+        .then(() => { 
+            self.skipWaiting() 
+        })
+    .catch(err => console.log('Hubo un error', err)) })); });
 
 self.addEventListener('activate', e => {
     const cacheWhitelist = [CACHE_NAME];
